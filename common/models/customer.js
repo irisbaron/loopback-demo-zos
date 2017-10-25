e5d195c32c3576c4ccb56f6e706ebdc1a00d2411
Muntasir Mallick <mmallick@ca.ibm.com>
2017-10-25 17:19:19 -0400

    Rewards program membership check
    Check to see that members are enrolled in the same
    membership program prior to aggregating their points

--- common/models/customer.js
+++ common/models/customer.js
@@ -1,14 +1,18 @@
 'use strict';
 
 module.exports = function(Customer) {
-  var app = require("../../server/server");
-  
-  Customer.getMemberId = function(MemberName) {
+   var app = require("../../server/server");
+   
+   Customer.getMemberId = function(MemberName) {
      var Customers = app.models.Customer;
      return Customers.find({where:{Name:MemberName}})
-  }
+   }
+   
+   Customer.getId = function(Member){
+     return Member.id;
+   } 
   
-  Customer.getId = function(Member){
-    return Member.id;
-  }
+   Customer.getPgmId = function(Member) {
+     return Member.programId;
+   }
 };
--- common/models/rewards-program.js
+++ common/models/rewards-program.js
@@ -1,20 +1,19 @@
 'use strict';
 
 module.exports = function(Rewardsprogram) {
-  var app = require("../../server/server");
-  var async = require("async");
+   var app = require("../../server/server");
+   var async = require("async");
   
-  Rewardsprogram.getPoints = function (Members,ProgramType,cb){
-    var Customers = app.models.Customer;
-    var CreditCards = app.models.CreditCard;
-
+   Rewardsprogram.getPoints = function (Members,ProgramType,cb){
+     var Customers = app.models.Customer;
+     var CreditCards = app.models.CreditCard;
     
      function queryMemberId(Members) {
-      return Promise.all(Members.map(Customers.getMemberId));
+          return Promise.all(Members.map(Customers.getMemberId));
      }
 
      function queryCreditCards(MemberIds) {
-       return Promise.all(MemberIds.map(CreditCards.getCards)); 
+         return Promise.all(MemberIds.map(CreditCards.getCards)); 
      } 
 
      function flattenRecords(queryResults, dataAccessor) {
@@ -30,12 +29,19 @@ module.exports = function(Rewardsprogram) {
      }
 
 
-    /*Query Customers Credit Card Information, collect all the points 
-      aggregate and return the sum
+    /*Query Customers Information, check to see if they belong to the same rewardProgram
+      then collect all the points, aggregate and return the sum
     */
      queryMemberId(Members)
      .then(function(queryResults){
-           var memberIds = flattenRecords(queryResults,Customers.getId)
+           var rewardPgmIds = flattenRecords(queryResults,Customers.getPgmId);
+           var referenceId = rewardPgmIds.pop();
+           var memberSet = rewardPgmIds.filter(id => {
+               return id == referenceId;
+               });
+           if (memberSet.length == 0)
+               cb(null,0);
+           var memberIds   = flattenRecords(queryResults,Customers.getId);
            queryCreditCards(memberIds)
            .then(function(queryResults) {
                    var points = flattenRecords(queryResults,CreditCards.getPoints);
@@ -44,9 +50,14 @@ module.exports = function(Rewardsprogram) {
                        });
                    cb(null,totalPoints);
            })
+           .catch(function(error) {
+                  console.log("Error occured in CreditCard query " + error);    
+           })
      })
-  
-  };
+     .catch(function(error) {
+            console.log("Error occured in Customer query" + error);
+     })
+   };
 
 
    Rewardsprogram.remoteMethod("getPoints",{
